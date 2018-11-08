9b298ed6ca58f35b71ea27fe37595fca9022dd96
Rafael Corrêa Gomes <rafaelcg_stz@hotmail.com>
2018-11-08 10:00:45 -0500

    Disabling Magento Version module

--- deploy.php
+++ deploy.php
@@ -114,6 +114,7 @@ task('magento:cache:flush', function () {
 desc('Enable allow symlink config in Magento Panel');
 task('magento:config', function () {
 	run("cd {{release_path}} && {{php}} {{magerun}} config:store:set dev/template/allow_symlink 1 {{verbose}}");
+	run("cd {{release_path}} && {{php}} {{release_path}}{{magento_bin}} module:disable Magento_Version {{verbose}}");
 	if(get('is_production')){
 		run("cd {{release_path}} && {{php}} {{magerun}} config:store:set design/search_engine_robots/default_robots INDEX,FOLLOW {{verbose}}");
 	}else{
