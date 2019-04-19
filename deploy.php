538a5cb2c9b6a0348b27a6469c4d1aa329fedef7
Rafael Corrêa Gomes <rafaelcg_stz@hotmail.com>
2019-04-19 16:48:53 -0400

    Check Magento version if supports feature upgrade

--- deploy.php
+++ deploy.php
@@ -120,17 +120,27 @@ task('magento:maintenance:disable', function () {
 desc('Upgrade magento database');
 task('magento:upgrade:db', function () {
 
-    // Check if need update DB
-    $isDbUpdated = test('[ "$({{php}} {{release_path}}{{magento_bin}} setup:db:status --no-ansi -n)" == "All modules are up to date." ]');
+    $supports = test('(( $(echo "{{magento_version}} 2.1" | awk \'{print ({{magento_version}} > 2.1)}\') ))');
 
-    if (!$isDbUpdated) {
-        run("if [ -d $(echo {{release_path}}{{magento_dir}}bin) ]; then {{php}} {{release_path}}{{magento_bin}} maintenance:enable {{verbose}}; fi");
-        run("{{php}} {{magerun}} setup:upgrade --keep-generated --root-dir={{release_path}} {{verbose}}");
+    if (!$supports) {
+        invoke('magento:maintenance:enable');
+        run("{{php}} {{release_path}}{{magento_bin}} setup:upgrade --keep-generated {{verbose}}");
         run("{{php}} {{magerun}} sys:setup:downgrade-versions --root-dir={{release_path}} {{verbose}}");
-        run("if [ -d $(echo {{release_path}}{{magento_dir}}bin) ]; then {{php}} {{release_path}}{{magento_bin}} maintenance:disable {{verbose}}; fi");
+        invoke('magento:maintenance:disable');
+    } else {
+        // Check if need update DB
+        $isDbUpdated = test('[ "$({{php}} {{release_path}}{{magento_bin}} setup:db:status --no-ansi -n)" == "All modules are up to date." ]');
+        if (!$isDbUpdated) {
+            write("All modules are up to date.");
+            invoke('magento:maintenance:enable');
+            run("{{php}} {{release_path}}{{magento_bin}} setup:upgrade --keep-generated {{verbose}}");
+            run("{{php}} {{magerun}} sys:setup:downgrade-versions --root-dir={{release_path}} {{verbose}}");
+            invoke('magento:maintenance:disable');
+        }else{
+            write("All modules are up to date.");
+        }
     }
-    write("All modules are up to date.");
-    
+
 });
 
 desc('Flush Magento Cache');
